c1de82f734232454004c98572fa620e6f6306561
YuriyNik156 <yuriy.nik156@gmail.com>
2025-05-30 18:59:55 +0300

    Update main.py

---
+++ lesson/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return("Hello World!")
+
+@app.route("/new/")
+@app.route("/newpage/")
+@app.route("/новаястраница/")
+def new():
+    return "new page"
+
+if __name__ == "__main__":
+    app.run()
