612d9e9ddc5b874991ef575cd4c5113f41a4cfb9
YuriyNik156 <yuriy.nik156@gmail.com>
2025-05-30 19:10:41 +0300

    add if and else

--- lesson/main.py
+++ lesson/main.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return("Hello World!")
+@app.route("/<password>/")
+def hello_world(password = None):
+    if password == "1234":
+        return f"Доступ разрешен"
+    else:
+        return f"Доступ запрещен"
 
 @app.route("/new/")
 @app.route("/newpage/")
